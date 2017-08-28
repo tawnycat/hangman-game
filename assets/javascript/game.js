b73cd501d9a23bf20bb659b6d07b38facc042c93
tawnycat <tawnyeleanor@gmail.com>
2017-08-28 19:46:07 -0400

    created array

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// Create array of movie stars
+
+var movieStars = [
+	"Humphrey Bogart", "James Stewart", "Fred Astaire", "Clark Gable", "Charlie Chaplin", "Gregory Peck", "John Wayne", "Gene Kelly", "Orson Welles", "James Dean", "Buster Keaton", "Sidney Poitier", "Katharine Hepburn", "Bette Davis", "Audrey Hepburn", "Greta Garbo", "Marilyn Monroe", "Elizabeth Taylor", "Judy Garland", "Marlene Dietrich", "Joan Crawford", "Grace Kelly", "Rita Hayworth", "Lauren Bacall", "Jean Harlow"
+]
+
+// Randomly choose one movie star
+
+// Display the empty characters for the user
+
+// Recognize that the user is hitting a key
+
+// Recognize whether this is a winning or losing guess
+
+// Add winning guess to name
+
+// Add losing guess to bad guess list
+
+// Add winning and losing guesses to list of already guessed letters
+
+// Allow three bad guesses before a loss
+
+// If they win or lose, add one to appropriate score
+
+// Display either "You win!" or "You lose"
+
+// Display photo of the movie star at the end of the game
+
+// Reset the game by pressing any key
+
